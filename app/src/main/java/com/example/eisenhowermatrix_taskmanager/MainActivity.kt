8acfe4c68b6fd74a1c81f7a6aaa37f4df28e756b
Marcus <reveriejacket@hotmail.com>
2024-09-17 12:18:48 -0300

    Layout basico da tela inicial (ainda sem comportamento)

--- app/src/main/java/com/example/eisenhowermatrix_taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/eisenhowermatrix_taskmanager/MainActivity.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.layout.systemBars
+import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.eisenhowermatrix_taskmanager.ui.theme.EisenhowerMatrixTaskManagerTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +33,78 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             EisenhowerMatrixTaskManagerTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                DashboardScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun DashboardScreen() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFF6F7FB)) // Background cinza claro da tela
+            .padding(12.dp) // Distância das bordas da tela
+            .windowInsetsPadding(WindowInsets.systemBars), //
+        verticalArrangement = Arrangement.spacedBy(16.dp) // Espaçamento entre os cartões
+    ) {
+        DashboardCard(
+            backgroundColor = Color(0xFF64D37E), // Verde
+            title = "Faça agora!",
+            subtitle = "Urgente e Importante"
+        )
+        DashboardCard(
+            backgroundColor = Color(0xFF4F96F1), // Azul
+            title = "Decida quando fazer!",
+            subtitle = "Importante, Não Urgente"
+        )
+        DashboardCard(
+            backgroundColor = Color(0xFFFFAA38), // Amarelo
+            title = "Delegue!",
+            subtitle = "Urgente, Não Importante"
+        )
+        DashboardCard(
+            backgroundColor = Color(0xFFFF6E5B), // Vermelho
+            title = "Elimine!",
+            subtitle = "Não importante, Não Urgente"
+        )
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    EisenhowerMatrixTaskManagerTheme {
-        Greeting("Android")
+fun DashboardCard(
+    backgroundColor: Color,
+    title: String,
+    subtitle: String
+    // precisa ter um valor para o número de tarefas
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(100.dp),
+        shape = RoundedCornerShape(16.dp),
+        colors =  CardDefaults.cardColors(
+            containerColor = backgroundColor)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Top
+        ) {
+            Text(
+                text = title,
+                style = MaterialTheme.typography.headlineMedium,
+                color = Color.White,
+                textAlign = TextAlign.Start
+            )
+            Text(
+                text = subtitle,
+                style = MaterialTheme.typography.bodyMedium,
+                color = Color.White
+            )
+
+        }
     }
 }
